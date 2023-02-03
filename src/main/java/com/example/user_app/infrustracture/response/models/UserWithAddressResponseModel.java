101ef59673d72b5f0f3c9b382a0b67db9e28cbfa
AshikurRahaman <ashikur.rahaman.cse@ulab.edu.bd>
2023-02-03 23:58:45 +0600

    test added

--- src/main/java/com/example/user_app/infrustracture/response/models/UserWithAddressResponseModel.java
+++ src/main/java/com/example/user_app/infrustracture/response/models/UserWithAddressResponseModel.java
@@ -3,6 +3,7 @@ package com.example.user_app.infrustracture.response.models;
 import com.example.user_app.core.model.UserWithAddressDomainModel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserWithAddressResponseModel {
     private List<UserWithAddressDomainModel> userInfoWithAddress;
 }
